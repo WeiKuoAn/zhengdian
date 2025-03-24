5b043f59ff7134d1ec6a4e04af3cb9cd60525273
魏國安 <qaz898970@gmail.com>
2025-03-24 14:16:45 +0800

    20250324複製派工修改

--- resources/views/task/copy.blade.php
+++ resources/views/task/copy.blade.php
@@ -107,13 +107,16 @@
                                     <input type="text" class="form-control" name="created_by" value="{{ Auth::user()->name }}"  readonly>
                                 </div>
                                 <div class="mb-3">
-                                    <label for="project-priority" class="form-label">優先序<span
+                                    <label for="project-priority" class="form-label">狀態<span
                                             class="text-danger">*</span></label>
-                                    <select class="form-control" data-toggle="select" data-width="100%" name="priority">
-                                        <option value="0" {{ $data->priority == 0 ? 'selected' : '' }}>緊急</option>
-                                        <option value="1" {{ $data->priority == 1 ? 'selected' : '' }}>高</option>
-                                        <option value="2" {{ $data->priority == 2 ? 'selected' : '' }}>中</option>
-                                        <option value="3" {{ $data->priority == 3 ? 'selected' : '' }}>低</option>
+                                    <select class="form-control" data-toggle="select" data-width="100%" name="status">
+                                        <option value="1" {{ $data->status == 1 ? 'selected' : '' }}>送出派工</option>
+                                        <option value="2" {{ $data->status == 2 ? 'selected' : '' }}>接收派工</option>
+                                        <option value="3" {{ $data->status == 3 ? 'selected' : '' }}>進行中</option>
+                                        <option value="4" {{ $data->status == 4 ? 'selected' : '' }}>移轉</option>
+                                        <option value="8" {{ $data->status == 8 ? 'selected' : '' }}>人員已完成，待確認
+                                        </option>
+                                        <option value="9" {{ $data->status == 9 ? 'selected' : '' }}>完成</option>
                                     </select>
                                 </div>
                                 
