97556de5b68fd754b31d7795b968a2bc85e79515
魏國安 <qaz898970@gmail.com>
2025-03-31 08:56:33 +0800

    新增SBIR controller

---
+++ app/Http/Controllers/SBIRController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SBIRController extends Controller
+{
+    public function sbir01()
+    {
+        return view('sbir.sbir01');
+    }
+
+    public function sbir02()
+    {
+        return view('sbir.sbir02');
+    }
+
+    public function sbir03()
+    {
+        return view('sbir.sbir03');
+    }
+    
+    public function sbir04()
+    {
+        return view('sbir.sbir04');
+    }
+
+    public function sbir05()
+    {
+        return view('sbir.sbir05');
+    }
+    
+    public function sbir06()
+    {
+        return view('sbir.sbir06');
+    }
+}
